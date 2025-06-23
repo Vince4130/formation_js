7770a2674754aa6789121fe02d95aae06c5c8f34
Vince4130 <vinceblanca@gmail.com>
2025-06-23 14:13:55 +0200

    Proto function toCapitalize

--- js/proto.js
+++ js/proto.js
@@ -1,3 +1,16 @@
-Number.prototype.even = function () {
+Number.prototype.even = function() {
     return this.valueOf() % 2 === 0;
+}
+
+String.prototype.toCapitalize = function() {
+    
+    var chaine = this.valueOf();
+
+    var words = chaine.split(' ');
+
+    for(var i=0; i < words.length; i++) {
+        words[i] = words[i].slice(0,1).toUpperCase() + words[i].slice(1);
+    }
+    
+    return words.join(' ')
 }
