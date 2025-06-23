57b3bff7184e41a4c778d20edce5c6b43c0f99bc
Vince4130 <vinceblanca@gmail.com>
2025-06-23 11:47:43 +0200

    Formation JS m2i

---
+++ js/02-first-step.js
@@ -0,0 +1,11 @@
+
+//document.write('Ce texte est écrit par une instruction Javascript.');
+document.write('Ce texte est écrit par l\'instruction JavaScript.');
+alert('Ceci est une alerte');
+console.log('Texte affiché dans la console');
+
+//document.write('Bonjour');
+//alert('Hello');
+//console.log('Buenos dias');
+
+       
